d799c70029013bc4cbe9a4d9e4124fc94742edd4
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-12-09 15:39:02 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col w-screen h-screen justify-center items-center">
+      <h1 className="text-8xl">Hello world!</h1>
+      <h3 className="text-2xl">Tobias Schmid</h3>
+    </div>
+  );
+}
