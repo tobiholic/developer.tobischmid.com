ebf27bc73e2ac92cd51d5655cfc01e7149997153
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-12-15 14:31:40 +0100

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,12 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const staticRoutes: MetadataRoute.Sitemap = [
     {
       url: 'https://developer.tobischmid.com/',
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(), // ISO-String für statische Daten
       changeFrequency: 'weekly',
       priority: 1,
     },
